c0fbc690c9705686636f781bb0437d958b68e957
Panagiotis <p.mastoukas@indice.gr>
2025-07-28 12:47:51 +0300

    Shared Sidebar for Teams

---
+++ internship.features.core/ViewComponents/TeamSidebarViewComponent.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class TeamSidebarViewComponent : ViewComponent
+{
+    private readonly ITeamService _teamService;
+
+    public TeamSidebarViewComponent(ITeamService teamService)
+    {
+        _teamService = teamService;
+    }
+
+    public async Task<IViewComponentResult> InvokeAsync()
+    {
+        var teams = await _teamService.GetAllTeamsAsync(); 
+        return View(teams);
+    }
+}
---
+++ internship.features.core/Views/Shared/Components/TeamSidebar/Default.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace internship.features.core.Views.Shared.Components.TeamSidebar
+{
+    public class DefaultModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
